17228ca157f314a26a0300754c05dc5a3a5fa646
Ryan Cervantes <ryanzcervantes@gmail.com>
2020-05-15 14:00:39 -0400

    Moved Module Intervals to Scripts
    I think waybar keeps spawning instances so moving it to the script will ensure that only one
    instance of the script is run at a time. It could just be that reloading sway just spawns instances
    without killing the old scripts. Regardless, this makes waybar do one execution for sure and then
    just waits on the script to give output

--- waybar/modules/bluetooth.sh
+++ waybar/modules/bluetooth.sh
@@ -1,28 +1,33 @@
 #! /bin/zsh
 
-running=$(systemctl status bluetooth | grep running | wc -l | xargs)
-non_blocked=$(rfkill -r | grep hci0 | wc -l | xargs)
-# If not active display nothing
-if [ "$running" -eq "1" ] && [ "$non_blocked" -eq "1" ]; then
-    connection=$(echo "info" | bluetoothctl | grep "Name" | awk -F": " '{print $2}')
-    count=$(echo "info" | bluetoothctl | grep "Name" | awk -F": " '{print $2}' |wc -l)
+while :
+do
+    running=$(systemctl status bluetooth | grep running | wc -l | xargs)
+    non_blocked=$(rfkill -r | grep hci0 | wc -l | xargs)
+    # If not active display nothing
+    if [ "$running" -eq "1" ] && [ "$non_blocked" -eq "1" ]; then
+        connection=$(echo "info" | bluetoothctl | grep "Name" | awk -F": " '{print $2}')
+        count=$(echo "info" | bluetoothctl | grep "Name" | awk -F": " '{print $2}' |wc -l)
 
-    # If active then check if there are any connections
-    if [ "$count" -gt "0" ]; then
-        text=" $connection"
-        percentage=100
-        class=("on")
+        # If active then check if there are any connections
+        if [ "$count" -gt "0" ]; then
+            text=" $connection"
+            percentage=100
+            class=("on")
+        else
+            text=' on'
+            percentage=50
+            class=("off")
+        fi
     else
-        text=' disconnected'
-        percentage=50
-        class=("off")
+        # If isn't running then dont display the icon
+        text=' off'
+        percentage=0
+        class=("down")
     fi
-else
-    # If isn't running then dont display the icon
-    text=' off'
-    percentage=0
-    class=("down")
-fi
 
-out="{\"text\": \"$text\", \"percentage\":$percentage, \"class\":[\"$class\"]}"
-echo $out
+    out="{\"text\": \"$text\", \"percentage\":$percentage, \"class\":[\"$class\"]}"
+    echo $out
+    sleep 30
+
+done
