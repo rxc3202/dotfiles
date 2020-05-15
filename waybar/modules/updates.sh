c04afedb782ff5cc5706d3dac43b39375b5c0397
Ryan Cervantes <ryanzcervantes@gmail.com>
2020-05-15 11:17:10 -0400

    update module working

--- waybar/modules/updates.sh
+++ waybar/modules/updates.sh
@@ -1,7 +1,20 @@
 #! /bin/sh
 # https://github.com/Bonnee/dotfiles/blob/home_as_root/.config/waybar/scripts/updates
+title="pacman-update-confirm"
+script="$HOME/.config/waybar/scripts/update-packages.sh"
 
-updates=$(pacman -Qu | wc -l)
-if [ "$updates" -gt "0" ]; then
-echo "$updates"
+if [ "$#" -eq "1" ] && [ "$1" = "--update" ]; then
+    if [ "$(swaymsg -t get_tree | grep $title |wc -l)" -eq "0" ]; then
+        alacritty -d 100 100 --title $title -e $script
+        exit 0
+    fi
+fi
+
+if [ "$#" -eq "1" ] && [ "$1" = "--fetch" ]; then
+    updates=$(pacman -Qu | awk -F"->" '{print $1}' | cut -d" " -f1)
+    num_updates=$(pacman -Qu | wc -l)
+    out="{\"text\": \"$num_updates\", \"tooltip\": \"New Versions: $updates\"}"
+    if [ "$num_updates" -gt "0" ]; then
+        echo $out
+    fi
 fi
---
+++ waybar/scripts/update-packages.sh
@@ -0,0 +1,15 @@
+#! /bin/zsh
+
+# Update the Packages
+echo "Packages Upgraded:"
+pacman -Qu
+#sudo pacman -Syu  >> /dev/null
+
+# Recommend Restart
+echo "Would you like to restart? [Y/n]"
+read response -n1 -r
+
+if [ $response == "Y" ]; then
+    sudo shutdown -r 0
+fi
+
